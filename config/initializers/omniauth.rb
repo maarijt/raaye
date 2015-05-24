cf4483a6179a54eacfde87078839e9d10b1ece26
maarijt <doombuggy101@gmail.com>
2015-05-24 12:41:16 +0500

    trying something

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,5 +4,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   configure do |config|
     config.path_prefix = '/auth'
   end
-  provider :facebook, "343918805807110","695007695f0143b9d392300f53470d78" , {:scope => 'user_about_me', :client_options => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
+  provider :facebook, "343918805807110","695007695f0143b9d392300f53470d78" , {:scope => 'user_about_me', :client_options => {:ssl => {:verify => false}}}
 end
